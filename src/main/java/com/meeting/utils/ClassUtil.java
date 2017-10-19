669b6554c537f2b4201536e18cc51e665747804a
FlowingFire <491643528@qq.com>
2017-10-19 21:13:22 +0800

    不抛异常

--- src/main/java/com/meeting/utils/ClassUtil.java
+++ src/main/java/com/meeting/utils/ClassUtil.java
@@ -211,7 +211,7 @@ public class ClassUtil {
         try {
             CommandIn command=(CommandIn)Class.forName(refereceName).newInstance();
             return command;
-        } catch (Exception e) {
+        } catch (Throwable e) {
             System.out.println("指令名输入错误，请参考帮助");
            HelpUtil.printHelp();
            return null;
